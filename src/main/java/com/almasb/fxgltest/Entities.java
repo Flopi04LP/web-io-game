efd1cb1ae009be6f244eafe7a05f85c64d55d412
Florian Büchi <54060167+Flopi04LP@users.noreply.github.com>
2021-06-07 08:19:56 +0200

    Update Entities.java

--- src/main/java/com/almasb/fxgltest/Entities.java
+++ src/main/java/com/almasb/fxgltest/Entities.java
@@ -2,7 +2,7 @@ package com.almasb.fxgltest;
 
 /**
  *
- * @author kappe
+ * @author Florian Büchi
  */
 public enum Entities {
     PLAYER, ENEMY, PROJECTILE, ENEMY_PROJECTILE, FOOD_RECTANGLE, FOOD_TRIANGLE, FOOD_OCTAGON, MED_BIG, MED_SMALL, ENEMY_TRIANGLE
